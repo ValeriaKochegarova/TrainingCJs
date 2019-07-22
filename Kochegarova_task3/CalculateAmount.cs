0dd3fe8fb09e4373b178cb5cc7b34735247d2152
Valeriya Kochegarova <vkochegarova@Ascendix.net>
2019-07-22 10:55:59 +0300

    added plugin files

---
+++ Kochegarova_task3/CalculateAmount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace Microsoft.Crm.Sdk.Samples
+{
+    public class CalculateAmount : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            Entity child = (Entity)context.InputParameters["Target"];
+            EntityReference parentRef = child.GetAttributeValue<EntityReference>("new_parentid");
+
+            string fetchXml = @"<fetch top='50' >
+                  <entity name='new_kochegarova2019_child' >
+                    <attribute name='new_amount' />
+                    <filter type='and' >
+                      <condition attribute='new_parentid' operator='eq' value='{0}' />
+                    </filter>
+                  </entity>
+                </fetch>";
+
+            fetchXml = string.Format(fetchXml, parentRef.Id);
+            EntityCollection res = service.RetrieveMultiple(new FetchExpression(fetchXml));
+
+
+            tracingService.Trace("child count:" + res.Entities.Count);
+            decimal total = 0;
+            foreach (var item in res.Entities)
+            {
+                Money amount = item.GetAttributeValue<Money>("new_amount");
+                total += amount.Value;
+            }
+            tracingService.Trace("total amount:" + total);
+            Entity parent = new Entity("new_kochegarova2019", parentRef.Id);
+            parent["new_amount"] = new Money(total);
+
+            service.Update(parent);
+
+        }
+    }
+}
---
+++ Kochegarova_task3/ParentLastNamePlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace Microsoft.Crm.Sdk.Samples
+{
+    public class CreatePluginForLastName : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            Entity child = (Entity)context.InputParameters["Target"];
+            EntityReference parentRef = child.GetAttributeValue<EntityReference>("new_parentid");
+            if (parentRef != null)
+            {
+                var parentRecord = service.Retrieve("new_kochegarova2019", parentRef.Id, new Xrm.Sdk.Query.ColumnSet("new_lastname"));
+                string lastNameFromParentEntity = parentRecord.GetAttributeValue<string>("new_lastname");
+                child["new_lastname"] = lastNameFromParentEntity;
+                tracingService.Trace("lastNameFromParentEntity:" + lastNameFromParentEntity);
+            }
+            service.Update(child);
+
+        }
+    }
+}
